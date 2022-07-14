b399cf0333820e6214f7c5dc57ed26c7f0656fbb
Daniela Barrios <dani_bj1@hotmail.com>
2022-07-14 13:25:45 -0300

    Agregar archivos de proyecto.

---
+++ cartasEspaniolas/ClassCartas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cartasEspaniolas
+{
+    internal class Carta
+    {
+        public string Palo { get ; set; }
+
+        public int Numero { get; set; }
+
+        public Carta()
+        {
+        }
+        public Carta(int numero, string palo)
+        {   
+        }
+        public void siguienteCarta(List<Carta>Baraja, List<Carta> Monton)
+        {
+            Carta siguiente = Baraja[0];
+            Console.WriteLine($"{siguiente.Numero} {siguiente.Palo}");
+            Monton.Add(siguiente);
+            Baraja.Remove(siguiente);
+        }
+        public void Mezclar(List<Carta> Baraja)
+        {
+            Baraja = Baraja.OrderBy(item => new Random().Next()).ToList();
+            foreach(var carta in Baraja)
+            {
+                Console.WriteLine(carta);
+            }
+        }
+        public void darCartas(List<Carta>Baraja, List<Carta> Monton)
+        {
+            Console.WriteLine("¿Cuantas cartas repartir a cada uno?");
+            var cartasRecibidas = int.Parse(Console.ReadLine());
+            if (cartasRecibidas < Baraja.Count)
+            {
+                for (int i = 0; i < cartasRecibidas; i++)
+                {
+                    Carta recibida = Baraja[i];
+                    Console.WriteLine($"{ recibida.Numero} { recibida.Palo}");
+                }
+            }
+        }
+    }
+}
---
+++ cartasEspaniolas/Program.cs
@@ -0,0 +1,21 @@
+﻿using cartasEspaniolas;
+
+List<Carta>Mazo = new List<Carta>();
+string[] palo = { "Espada", "Basto", "Oro", "Copa" };
+for(int p= 0; p<4; p++)
+{
+    for(int c=0; c<12; c++)
+    {
+        var paloBaraja =palo[p];
+        if(c==7 || c == 8)
+        {
+        }
+        else
+        {
+            Carta nuevaCarta = new Carta(c+1, paloBaraja);
+            Mazo.Add(nuevaCarta);   
+        }
+    }
+}
+
+    
